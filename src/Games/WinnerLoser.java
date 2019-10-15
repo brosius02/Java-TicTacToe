87a86eaf53124fad39759f0a4deb3047c2cd601f
Brosius John <brosiusj@davenportschools.org>
2019-10-15 13:21:39 -0500

    fix checker board
    Checker board started with 0 now starts with 1

--- src/Games/WinnerLoser.java
+++ src/Games/WinnerLoser.java
@@ -28,22 +28,17 @@ public class WinnerLoser {
 
         int oWins = 0;
         int xWins = 1;
-        if (location.get(0).equals(check) && location.get(1).equals(check) && location.get(2).equals(check)) {
+        if (location.get(1).equals(check) && location.get(2).equals(check) && location.get(3).equals(check)) {
             if(check.equalsIgnoreCase("O")){
                 oWins++; }
             else if(check.equalsIgnoreCase("X")){
                 xWins++; }
-        } else if (location.get(6).equals(check) && location.get(4).equals(check) && location.get(2).equals(check)) {
+        } else if (location.get(7).equals(check) && location.get(5).equals(check) && location.get(3).equals(check)) {
             if(check.equalsIgnoreCase("O")){
                 oWins++; }
             else if(check.equalsIgnoreCase("X")){
                 xWins++; }
-        } else if (location.get(6).equals(check) && location.get(7).equals(check) && location.get(8).equals(check)) {
-            if(check.equalsIgnoreCase("O")){
-                oWins++; }
-            else if(check.equalsIgnoreCase("X")){
-                xWins++; }
-        } else if (location.get(0).equals(check) && location.get(3).equals(check) && location.get(6).equals(check)) {
+        } else if (location.get(7).equals(check) && location.get(8).equals(check) && location.get(9).equals(check)) {
             if(check.equalsIgnoreCase("O")){
                 oWins++; }
             else if(check.equalsIgnoreCase("X")){
@@ -58,12 +53,17 @@ public class WinnerLoser {
                 oWins++; }
             else if(check.equalsIgnoreCase("X")){
                 xWins++; }
-        } else if (location.get(3).equals(check) && location.get(4).equals(check) && location.get(5).equals(check)) {
+        } else if (location.get(3).equals(check) && location.get(6).equals(check) && location.get(9).equals(check)) {
             if(check.equalsIgnoreCase("O")){
                 oWins++; }
             else if(check.equalsIgnoreCase("X")){
                 xWins++; }
-        } else if (location.get(0).equals(check) && location.get(4).equals(check) && location.get(8).equals(check)) {
+        } else if (location.get(4).equals(check) && location.get(5).equals(check) && location.get(6).equals(check)) {
+            if(check.equalsIgnoreCase("O")){
+                oWins++; }
+            else if(check.equalsIgnoreCase("X")){
+                xWins++; }
+        } else if (location.get(1).equals(check) && location.get(5).equals(check) && location.get(9).equals(check)) {
             if(check.equalsIgnoreCase("O")){
                 oWins++; }
             else if(check.equalsIgnoreCase("X")){
